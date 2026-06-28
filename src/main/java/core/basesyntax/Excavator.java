80e224b639705b67595ebafde495a34a7f0fe2b5
vadym-bondar-g <evansfalter@gmail.com>
2026-06-28 11:21:28 +0200

    feat: implements classes Bulldozer, Excavator,Truck

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Excavator is start to work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Bulldozer is stop to work");
+    }
+}
